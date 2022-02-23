2f7dec9fd0dbb341eca3ebeec8f80492fe222344
harry <tlqfk__@naver.com>
2022-02-23 15:06:35 +0900

    22-02-23

---
+++ algorithm/src/backjoon/bj5622.java
@@ -0,0 +1,35 @@
+package backjoon;
+
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class bj5622 {
+    public static void main(String[] args) throws IOException {
+
+        int count = 0;
+        int value;
+
+        while(true){
+            value = System.in.read();
+
+            if(value == '\n'){
+                break;
+            }
+
+            if(value < 68) count += 3;
+            else if(value < 71) count += 4;
+            else if(value < 74) count += 5;
+            else if(value < 77) count += 6;
+            else if(value < 80) count += 7;
+            else if(value < 84) count += 8;
+            else if(value < 87) count += 9;
+            else count += 10;
+        }
+
+        System.out.println(count);
+
+    }
+}
