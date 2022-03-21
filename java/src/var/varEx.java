42b42fdeaf629fbc592da06984311ea5c1a4f314
harry <tlqfk__@naver.com>
2022-03-21 09:44:38 +0900

    22-03-21

---
+++ java/src/var/varEx.java
@@ -0,0 +1,26 @@
+package var;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class varEx {
+    public static void main(String[] args) {
+        var list = List.of(1,2,3,4,5);
+        for(var element:list){
+            System.out.println(element);
+        }
+
+        var list2 = List.of(1,"3",4,5,"abc");
+        for(var element:list2){
+            System.out.println(element);
+        }
+
+        /*var path = Path.of("/a.txt");
+        try(var stream = Files.newInputStream(path)){
+            System.out.println(path.getFileName());
+        }catch(Exception e){
+            e.printStackTrace();
+        }*/
+    }
+}
