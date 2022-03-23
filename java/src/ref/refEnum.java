4f47e257733e7f44476b1bb5e1b4ee233e84b389
harry <tlqfk__@naver.com>
2022-03-23 10:36:05 +0900

    22-03-23

---
+++ java/src/ref/refEnum.java
@@ -0,0 +1,27 @@
+package ref;
+
+public class refEnum {
+    public static void main(String[] args) {
+        enum color { RED,ORANGE,YELLOW,GREEN }
+
+        enum Rainbow{
+            RED(3),ORANGE(10),YELLOW(21),GREEN(5);
+            private final int value;
+            Rainbow(int value){
+                this.value = value;
+            }
+            public int getValue(){
+                return value;
+            }
+        }
+
+        System.out.println(Rainbow.RED.getValue());
+        Rainbow[] arr = Rainbow.values();
+        for(Rainbow rb : arr){
+            System.out.println(rb);
+        }
+
+        Rainbow rb = Rainbow.valueOf("GREEN");
+        System.out.println(rb);
+    }
+}
--- java/src/ref/refStringBuffer.java
+++ java/src/ref/refStringBuffer.java
@@ -2,6 +2,7 @@ package ref;
 
 public class refStringBuffer {
     public static void main(String[] args) {
+        //String is immutable but StringBuffer is mutable
         String a = "asd";
         a = "abc";
         System.out.println(a);
@@ -9,5 +10,27 @@ public class refStringBuffer {
         String b = new String("asd");
         b = "abc";
         System.out.println(b);
+
+        StringBuffer sb = new StringBuffer("java");
+        System.out.println(sb);
+
+        sb.append("class");
+        System.out.println(sb);
+        System.out.println(sb.capacity());
+
+        sb.append("harryaaaaaaa");
+        System.out.println(sb.capacity());
+
+        sb.delete(14,21);
+        System.out.println(sb);
+
+        sb.append("a");
+        System.out.println(sb);
+
+        sb.deleteCharAt(14);
+        System.out.println(sb);
+
+        sb.insert(4,"insert");
+        System.out.println(sb);
     }
 }
