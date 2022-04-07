b6138265f2c3da29e7ac67c917744265b3c535c7
harry <tlqfk__@naver.com>
2022-04-07 09:34:22 +0900

    22-04-07

--- java/src/ref/refOptional.java
+++ java/src/ref/refOptional.java
@@ -1,11 +1,19 @@
 package ref;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Optional;
 
 public class refOptional {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         Optional<String> opt = Optional.ofNullable("asd");
         System.out.println(opt.get());
+
+        SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm");
+        String a = timeFormat.format(new Date());
+        Date date = timeFormat.parse(a);
+        System.out.println(a);
 
     }
 }
