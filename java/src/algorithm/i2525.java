61d96d6a7a6f5bfc60539c3f06750d1defb97fd8
namhyunwoo <sksskdfg@gmail.com>
2022-04-20 17:58:38 +0900

    Wed Apr 20 17:58:38     2022

---
+++ java/src/algorithm/i2525.java
@@ -0,0 +1,24 @@
+package algorithm;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class i2525 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int hour = sc.nextInt();
+        int minute = sc.nextInt();
+        int plusMinute = sc.nextInt();
+        LocalTime lt = LocalTime.of(hour, minute).plusMinutes(plusMinute);
+        if (lt.getMinute() == 0) {
+            System.out.print(lt.format(DateTimeFormatter.ofPattern("HH m")));
+        } else if (lt.getHour() == 0) {
+            System.out.print(lt.format(DateTimeFormatter.ofPattern("H mm")));
+        } else if (lt.getMinute() == 0 && lt.getHour() == 0) {
+            System.out.print(lt.format(DateTimeFormatter.ofPattern("H m")));
+        } else {
+            System.out.print(lt.format(DateTimeFormatter.ofPattern("HH mm")));
+        }
+    }
+}
