735097bd9c76ed2e5bd1fe7f371bbd276a9d0057
harry <sksskdfg@gmail.com>
2022-06-08 11:02:26 +0900

    22-06-08 자바로 파이썬 len 구현해보기 --

---
+++ java/src/vartype/vartype.java
@@ -0,0 +1,13 @@
+package vartype;
+
+import static java.lang.System.out;
+import static vartype.lib.len;
+
+public class vartype {
+    public static void main(String[] args) {
+        String strings = "abcdef";
+        Integer a = 54;
+
+        out.print(len(a));
+    }
+}
